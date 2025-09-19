f05273bb471a0268766d693c31a6649b5f7ddbc6
Saroj Karki <karkisaroj3012@gmail.com>
2025-09-19 14:59:36 +0545

    Implement AddExpense screen with navigation and MoneyAdition widget

--- lib/Features/transaction/Core/add_expense.dart
+++ lib/Features/transaction/Core/add_expense.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:money_manager/Features/transaction/Presentation/transaction_screen.dart';
+import 'package:money_manager/Features/transaction/Widget/money_adition.dart';
+import 'package:page_transition/page_transition.dart';
 
 class AddExpense extends StatefulWidget {
   const AddExpense({super.key});
@@ -10,6 +13,25 @@ class AddExpense extends StatefulWidget {
 class _AddExpenseState extends State<AddExpense> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          tooltip: "Go Back",
+          onPressed: () {
+            Navigator.push(
+              context,
+              PageTransition(
+                type: PageTransitionType.leftToRight,
+                child: TransactionScreen(),
+              ),
+            );
+          },
+          icon: Icon(Icons.arrow_back_ios_new),
+        ),
+        title: Text("Expense"),
+        centerTitle: true,
+      ),
+      body: MoneyAdition(),
+    );
   }
 }
