82227d585b5d92fd2638a4d29188fbb764e886ef
Saroj Karki <karkisaroj3012@gmail.com>
2025-10-08 15:39:16 +0545

    Refactor AccountsScreen to use Bloc for state management and improve UI structure. Update Myapp to utilize MultiBlocProvider for dependency injection. Update pubspec.yaml and pubspec.lock to include flutter_bloc dependency.

--- lib/Features/Accounts/Presentation/accounts_screen.dart
+++ lib/Features/Accounts/Presentation/accounts_screen.dart
@@ -1,189 +1,207 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'bloc/accounts_bloc.dart';
+import 'bloc/accounts_state.dart';
 
 class AccountsScreen extends StatelessWidget {
   const AccountsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Accounts')),
-      body: ListView(
-        padding: const EdgeInsets.all(16),
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Account',
-                    style: TextStyle(fontSize: 16, color: Colors.blue),
-                  ),
-                  Text(
-                    '₹6,628.12',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue,
+    return BlocBuilder<AccountsBloc, AccountsState>(
+      builder: (context, state) {
+        if (state is AccountsLoaded) {
+          return Scaffold(
+            appBar: AppBar(title: const Text('Accounts')),
+            body: ListView(
+              padding: const EdgeInsets.all(16),
+              children: const [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Account',
+                          style: TextStyle(fontSize: 16, color: Colors.blue),
+                        ),
+                        Text(
+                          '₹6,628.12',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    'Liabilities',
-                    style: TextStyle(fontSize: 16, color: Colors.red),
-                  ),
-                  Text(
-                    '₹208,242.65',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.red,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Liabilities',
+                          style: TextStyle(fontSize: 16, color: Colors.red),
+                        ),
+                        Text(
+                          '₹208,242.65',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    'Total',
-                    style: TextStyle(fontSize: 16, color: Colors.white),
-                  ),
-                  Text(
-                    '-₹201,614.53',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Total',
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                        Text(
+                          '-₹201,614.53',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
                     ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Divider(thickness: 2),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    'Cash',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                ],
-              ),
-            ],
-          ),
-          const SizedBox(height: 20),
-          Divider(thickness: 2),
-          // Account section
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              'Cash',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Cash', style: TextStyle(fontSize: 16)),
+                    Text(
+                      '₹68.45',
+                      style: TextStyle(fontSize: 16, color: Colors.blue),
+                    ),
+                  ],
+                ),
+                Divider(),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    'Accounts',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('HIBD', style: TextStyle(fontSize: 16)),
+                    Text(
+                      '₹2,768.66',
+                      style: TextStyle(fontSize: 16, color: Colors.blue),
+                    ),
+                  ],
+                ),
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('RBO', style: TextStyle(fontSize: 16)),
+                    Text(
+                      '₹1,613.61',
+                      style: TextStyle(fontSize: 16, color: Colors.blue),
+                    ),
+                  ],
+                ),
+                Divider(),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    'Card',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('HIBD Travel', style: TextStyle(fontSize: 16)),
+                    Text(
+                      '₹0.00',
+                      style: TextStyle(fontSize: 16, color: Colors.blue),
+                    ),
+                  ],
+                ),
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('RBO Credit Card', style: TextStyle(fontSize: 16)),
+                    Text(
+                      '₹0.00',
+                      style: TextStyle(fontSize: 16, color: Colors.blue),
+                    ),
+                  ],
+                ),
+                Divider(),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    'Debit Card',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('HIBD Debit Card', style: TextStyle(fontSize: 16)),
+                    Text(
+                      '₹0.00',
+                      style: TextStyle(fontSize: 16, color: Colors.blue),
+                    ),
+                  ],
+                ),
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('RBO Debit Card', style: TextStyle(fontSize: 16)),
+                    Text(
+                      '₹0.00',
+                      style: TextStyle(fontSize: 16, color: Colors.blue),
+                    ),
+                  ],
+                ),
+                Divider(),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    'Savings',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('RBO Saving', style: TextStyle(fontSize: 16)),
+                    Text(
+                      '₹100.00',
+                      style: TextStyle(fontSize: 16, color: Colors.blue),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('Cash', style: TextStyle(fontSize: 16)),
-              Text(
-                '₹68.45',
-                style: TextStyle(fontSize: 16, color: Colors.blue),
-              ),
-            ],
-          ),
-          Divider(),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              'Accounts',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('HIBD', style: TextStyle(fontSize: 16)),
-              Text(
-                '₹2,768.66',
-                style: TextStyle(fontSize: 16, color: Colors.blue),
-              ),
-            ],
-          ),
-          Divider(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('RBO', style: TextStyle(fontSize: 16)),
-              Text(
-                '₹1,613.61',
-                style: TextStyle(fontSize: 16, color: Colors.blue),
-              ),
-            ],
-          ),
-          Divider(),
-          // Card section
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              'Card',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('HIBD Travel', style: TextStyle(fontSize: 16)),
-              Text('₹0.00', style: TextStyle(fontSize: 16, color: Colors.blue)),
-            ],
-          ),
-          Divider(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('RBO Credit Card', style: TextStyle(fontSize: 16)),
-              Text('₹0.00', style: TextStyle(fontSize: 16, color: Colors.blue)),
-            ],
-          ),
-          Divider(),
-          // Debit Card section
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              'Debit Card',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('HIBD Debit Card', style: TextStyle(fontSize: 16)),
-              Text('₹0.00', style: TextStyle(fontSize: 16, color: Colors.blue)),
-            ],
-          ),
-          Divider(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('RBO Debit Card', style: TextStyle(fontSize: 16)),
-              Text('₹0.00', style: TextStyle(fontSize: 16, color: Colors.blue)),
-            ],
-          ),
-          Divider(),
-          // Savings section
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              'Savings',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('RBO Saving', style: TextStyle(fontSize: 16)),
-              Text(
-                '₹100.00',
-                style: TextStyle(fontSize: 16, color: Colors.blue),
-              ),
-            ],
-          ),
-        ],
-      ),
+          );
+        }
+        return const Scaffold(body: Center(child: CircularProgressIndicator()));
+      },
     );
   }
 }
--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:money_manager/Main/home_screen.dart';
+import 'Features/Accounts/Presentation/bloc/accounts_bloc.dart';
+import 'Features/Stats/Presentation/bloc/stats_bloc.dart';
+import 'Features/transaction/Presentation/bloc/transaction_bloc.dart';
+import 'Features/Configuration/Presentation/bloc/configuration_bloc.dart';
 
 class Myapp extends StatelessWidget {
   const Myapp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.white,
-          brightness: Brightness.dark,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AccountsBloc()),
+        BlocProvider(create: (_) => StatsBloc()),
+        BlocProvider(create: (_) => TransactionBloc()),
+        BlocProvider(create: (_) => ConfigurationBloc()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.white,
+            brightness: Brightness.dark,
+          ),
+          useMaterial3: true,
         ),
-        useMaterial3: true,
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
     );
   }
 }
